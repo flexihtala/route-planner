6096181d741590829f10d0913eff359620f26bb4
flexihtala <flexihtala@gmail.com>
2024-11-30 23:09:12 +0500

    поменял айпи апи

--- web/static/script.js
+++ web/static/script.js
@@ -15,7 +15,7 @@ document.getElementById('searchButton').addEventListener('click', () => {
     place_type: placeTypes
   };
 
-  fetch('http://158.160.27.205:8080/api/places/search/', {
+  fetch('http://127.0.0.1:8080/api/places/search/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
